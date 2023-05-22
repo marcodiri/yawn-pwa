ac23f8b3ac51978658d5f88be334ea3b01191837
marcodiri <marcodiri96@gmail.com>
2023-05-22 10:30:14 +0200

    Added exercises list

--- src/components/LogExerciseModal.vue
+++ src/components/LogExerciseModal.vue
@@ -9,7 +9,17 @@
         <ion-title>Add exercise</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding">
+    <ion-content>
+      <ion-list>
+        <ion-item v-for="[key, ex] in exList" class="list-item" lines="full">
+          <ion-label>{{ ex.name }}</ion-label>
+          <ion-buttons slot="end">
+            <ion-button class="btn-add-to-log" aria-label="add-exercise-to-log" @click="">
+              <ion-icon slot="icon-only" :icon="addCircleOutline" aria-hidden="true"></ion-icon>
+            </ion-button>
+          </ion-buttons>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-modal>
 </template>
@@ -26,14 +36,14 @@ import {
   IonBackButton,
   IonModal,
   IonItem,
-  IonInput,
-  actionSheetController,
-  IonSelect,
-  IonSelectOption,
+  IonList,
+  IonLabel,
+  IonIcon
 } from '@ionic/vue';
+import { addCircleOutline } from 'ionicons/icons';
 import { Ref, inject, ref, toRaw } from 'vue';
 
-import {attachHistoryListener, dismissModal} from '@/composables/modalBackButton';
+import { attachHistoryListener, dismissModal } from '@/composables/modalBackButton';
 
 defineProps<{
   trigger: string
@@ -43,3 +53,19 @@ defineProps<{
 const exList: Ref<Map<string, Exercise> | undefined> = inject('exercisesList')!;
 
 </script>
+
+<style scoped>
+.btn-add-to-log {
+  width: 100% !important;
+  height: 100% !important;
+  color: var(--ion-text-color);
+}
+
+.btn-add-to-log::part(native) {
+  padding: 0;
+}
+
+.btn-add-to-log ion-icon {
+  font-size: 28px;
+}
+</style>
